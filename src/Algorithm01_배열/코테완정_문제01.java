7afbb43ee298bb3eed0e09dff82d5e5ddd99566b
inwoo <jiw413@naver.com>
2024-06-11 11:25:24 +0900

    project init

---
+++ src/Algorithm01_배열/코테완정_문제01.java
@@ -0,0 +1,28 @@
+package Algorithm01_배열;
+
+/**
+ * 출처 : 코딩 테스트 완전 정복 - 문제 01 배열 정렬하기
+ */
+
+import java.util.*;
+
+public class 코테완정_문제01 {
+    public static void main(String[] args) {
+        // input data
+        int[][] arrays = {
+                {1, -5, 2, 4, 3},
+                {2, 1, 1, 3, 2, 5, 4},
+                {6, 1, 7}
+        };
+
+        // output data
+        for (int i = 0; i < arrays.length; i++) {
+            System.out.println(Arrays.toString(solution(arrays[i])));
+        }
+    }
+
+    private static int[] solution(int[] arr) {
+        Arrays.sort(arr);
+        return arr;
+    }
+}
