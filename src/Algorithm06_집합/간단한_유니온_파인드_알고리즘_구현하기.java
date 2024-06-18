acb3d4c44182fade7d5ca38466d03c8108b3b88e
inwoo <jiw413@naver.com>
2024-06-18 12:09:58 +0900

    수정 : 문제30 간단한 유니온-파인드 알고리즘 구현하기

--- src/Algorithm06_집합/간단한_유니온_파인드_알고리즘_구현하기.java
+++ src/Algorithm06_집합/간단한_유니온_파인드_알고리즘_구현하기.java
@@ -3,7 +3,7 @@ package Algorithm06_집합;
 /**
  * 출처 : 코딩 테스트 완전 정복 - 문제 30 간단한 유니온-파인드 알고리즘 구현하기
  * 일시 : 24.06.17(월)
- * 풀이 시간 : 50분
+ * 풀이 시간 : 0분
  */
 
 import java.util.*;
@@ -11,15 +11,19 @@ import java.util.*;
 public class 간단한_유니온_파인드_알고리즘_구현하기 {
     public static void main(String[] args) {
         // input data
-        int[][][] arrays = {
+        int[] k = {
+                3,
+                4
+        };
+        int[][][] operations = {
                 {{0, 0, 1}, {0, 1, 2}, {1, 1, 2}},
                 {{0, 0, 1}, {1, 1, 2}, {0, 1, 2}, {1, 0, 2}}
         };
 
         // output data
-        for (int i = 0; i < arrays.length; i++) {
+        for (int i = 0; i < operations.length; i++) {
             System.out.println(Arrays.toString(
-                    new 간단한_유니온_파인드_알고리즘_구현하기().solution(arrays[i])
+                    new 간단한_유니온_파인드_알고리즘_구현하기().solution(k[i], operations[i])
             ));
         }
     }
